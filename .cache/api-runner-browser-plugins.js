8332861692f29372e5fd95c9ae11b0443e6ef139
Jacob D. Castro <jdcastro.business@gmail.com>
2018-10-10 14:58:50 -0700

    gatsby build

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,1 +1,7 @@
-module.exports = []
+module.exports = [{
+      plugin: require('C:/Users/jdcas/jdcastro-web-dd/jdcastro-react/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('C:/Users/jdcas/jdcastro-web-dd/jdcastro-react/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
