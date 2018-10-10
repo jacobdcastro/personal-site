b8346df8171eb61c8fd0d62b2dcd42243feb0663
Jacob D. Castro <jdcastro.business@gmail.com>
2018-10-10 15:08:35 -0700

    gatsby develop command files

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,13 +1,3 @@
-var plugins = [{
-      plugin: require('C:/Users/jdcas/jdcastro-web-dd/jdcastro-react/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('C:/Users/jdcas/jdcastro-web-dd/jdcastro-react/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('C:/Users/jdcas/jdcastro-web-dd/jdcastro-react/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","icon":"src/images/jdc-favicon.png"},
-    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
