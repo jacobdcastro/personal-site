a984b6911f8c31733f4694ef9a35d53ee5a57592
Jacob D. Castro <jdcastro.business@gmail.com>
2019-05-23 19:44:53 -0700

    edit path for source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,64 +1,66 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
+  pathPrefix: '/blog',
+
   siteMetadata: {
-    title: "Jacob D. Castro",
+    title: 'Jacob D. Castro',
     description:
-      "A personal, informational website and design/development playground for Jacob D. Castro"
+      'A personal, informational website and design/development playground for Jacob D. Castro'
   },
-  pathPrefix: "/blog",
+
   plugins: [
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "Jacob D. Castro Personal website",
-        short_name: "JDCastro",
-        start_url: "/",
-        background_color: "#fafafa",
-        theme_color: "#353535",
+        name: 'Jacob D. Castro Personal website',
+        short_name: 'JDCastro',
+        start_url: '/',
+        background_color: '#fafafa',
+        theme_color: '#353535',
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: "minimal-ui",
-        icon: "src/images/favicon.png" // This path is relative to the root of the site.
+        display: 'minimal-ui',
+        icon: 'src/images/favicon.png' // This path is relative to the root of the site.
       }
     },
     {
-      resolve: "gatsby-plugin-styled-components",
+      resolve: 'gatsby-plugin-styled-components',
       options: {
         // Add any options here
       }
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: path.join(__dirname, "src", "images")
+        name: 'images',
+        path: path.join(__dirname, 'src', 'images')
       }
     },
     {
-      resolve: "gatsby-source-contentful",
+      resolve: 'gatsby-source-contentful',
       options: {
         //SPACE_ID build environment variable from Netlify or Gatsby?
-        spaceId: "oghc6wtiomc3",
+        spaceId: 'oghc6wtiomc3',
         //DELIVERY_API_TOKEN build environment variable from Netlify or Gatsby?
         accessToken:
-          "9f37d0ec8416ebe8c9c11b5db58b1a85d39c0f369f15fcc863b47f53c32179e7"
+          '9f37d0ec8416ebe8c9c11b5db58b1a85d39c0f369f15fcc863b47f53c32179e7'
       }
     },
 
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: "Jacob D. Castro Personal Website",
-        short_name: "personal",
-        start_url: "/",
-        icon: "src/images/jdc-favicon.png" // This path is relative to the root of the site.
+        name: 'Jacob D. Castro Personal Website',
+        short_name: 'personal',
+        start_url: '/',
+        icon: 'src/images/jdc-favicon.png' // This path is relative to the root of the site.
       }
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: "UA-130258530-1",
+        trackingId: 'UA-130258530-1',
         head: false,
         anonymize: true,
         respectDNT: true,
@@ -73,11 +75,11 @@ module.exports = {
         // cookieDomain: 'jacobdcastro.com',
       }
     },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-remark",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-offline",
-    "gatsby-plugin-lodash"
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-lodash'
   ]
 };
