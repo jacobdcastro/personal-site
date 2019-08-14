f95f4f222d0b79e91dacf54f6e691517b9ddc712
Jacob D. Castro <jdcastro.business@gmail.com>
2019-08-14 15:10:42 -0700

    update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,6 @@ module.exports = {
 
   siteMetadata: {
     title: 'Jacob D. Castro',
-    description:
-      'A personal, informational website and design/development playground for Jacob D. Castro'
   },
 
   plugins: [
@@ -21,41 +19,38 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: 'minimal-ui',
-        icon: 'src/images/favicon.png' // This path is relative to the root of the site.
-      }
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
+      },
     },
     {
       resolve: 'gatsby-plugin-styled-components',
       options: {
         // Add any options here
-      }
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: path.join(__dirname, 'src', 'images')
-      }
-    },
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        //SPACE_ID build environment variable from Netlify or Gatsby?
-        spaceId: 'oghc6wtiomc3',
-        //DELIVERY_API_TOKEN build environment variable from Netlify or Gatsby?
-        accessToken:
-          '9f37d0ec8416ebe8c9c11b5db58b1a85d39c0f369f15fcc863b47f53c32179e7'
-      }
+        path: path.join(__dirname, 'src', 'images'),
+      },
     },
 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: path.join(__dirname, 'content'),
+      },
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Jacob D. Castro Personal Website',
         short_name: 'personal',
         start_url: '/',
-        icon: 'src/images/jdc-favicon.png' // This path is relative to the root of the site.
-      }
+        icon: 'src/images/jdc-favicon.png', // This path is relative to the root of the site.
+      },
     },
     {
       resolve: 'gatsby-plugin-google-analytics',
@@ -71,15 +66,15 @@ module.exports = {
 
         // Any additional create only fields (optional)
         sampleRate: 5,
-        siteSpeedSampleRate: 10
+        siteSpeedSampleRate: 10,
         // cookieDomain: 'jacobdcastro.com',
-      }
+      },
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-transformer-remark',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-offline',
-    'gatsby-plugin-lodash'
-  ]
+    'gatsby-plugin-lodash',
+  ],
 };
