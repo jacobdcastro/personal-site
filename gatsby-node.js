5cb7193e95c06fde4e86cf482197255a13624281
Jacob D. Castro <jdcastro.business@gmail.com>
2019-08-19 17:27:59 -0700

    create pages with markdown files

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,72 +1,44 @@
-const path = require("path");
+const path = require('path');
 
-// exports.createPages = ({ graphql, actions }) => {
-//   const { createPage } = actions;
-//   return new Promise((resolve, reject) => {
-//     const blogPostTemplate = path.resolve("src/templates/BlogPost.js");
-//     resolve(
-//       graphql(`
-//         {
-//           allContentfulBlogPost(limit: 100) {
-//             edges {
-//               node {
-//                 id
-//                 title
-//                 slug
-//                 subtitle
-//                 published(formatString: "MMMM Do, YYYY")
-//                 bodyContent {
-//                   bodyContent
-//                 }
-//                 heroImage {
-//                   title
-//                   description
-//                   file {
-//                     url
-//                     fileName
-//                     contentType
-//                   }
-//                 }
-//                 bodyContent {
-//                   childMarkdownRemark {
-//                     html
-//                     excerpt(pruneLength: 77)
-//                   }
-//                 }
-//                 author {
-//                   id
-//                   name
-//                   email
-//                   birthday
-//                   avatar {
-//                     file {
-//                       url
-//                     }
-//                   }
-//                   biography {
-//                     biography
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `).then(result => {
-//         if (result.errors) {
-//           reject(result.errors);
-//         }
-//         result.data.allContentfulBlogPost.edges.forEach(edge => {
-//           createPage({
-//             path: `blog/${edge.node.slug}`,
-//             component: blogPostTemplate,
-//             context: {
-//               slug: edge.node.slug
-//             }
-//           });
-//         });
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { frontmatter: { blogPost: { eq: "blogPost" } } }
+      ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              title
+              date
+              slug
+              blogPost
+            }
+          }
+        }
+      }
+    }
+  `);
 
-//         return;
-//       })
-//     );
-//   });
-// };
+  if (result.errors) {
+    reject(result.errors);
+  }
+
+  const blogPostTemplate = path.resolve('src/templates/BlogPost.js');
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: `blog/${node.slug}`,
+      component: blogPostTemplate,
+      context: {
+        id: node.id,
+        slug: node.frontmatter.slug,
+      },
+    });
+  });
+
+  return;
+};
