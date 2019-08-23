9500ade7eb47e4dfd599028ed54baaec4df31eba
Jacob D. Castro <jdcastro.business@gmail.com>
2019-08-23 10:49:39 -0700

    Begin development of Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,15 @@
-// TODO start over
+import React from 'react';
+import Layout from '../templates/layout';
+import { Wrapper } from '../styles/IndexStyles';
+
+const Index = () => {
+  return (
+    <ThemeProvider>
+      <Layout>
+        <h1>Hello there</h1>
+      </Layout>
+    </ThemeProvider>
+  );
+};
+
+export default Index;
