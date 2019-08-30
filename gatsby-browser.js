6e28dfe7105b2c70fa543d516ff6c27b40789830
Jacob D. Castro <jdcastro.business@gmail.com>
2019-08-29 20:31:50 -0700

    add app wrappers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import ThemeContext from './src/utils/ThemeContext';
 import { ThemeProvider } from 'styled-components';
-import { themes } from './src/styles/__themes';
+import { darkTheme, lightTheme } from './src/styles/__themes';
 
+// provide theme state to entire app
 export const wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={themes.dark}>{element}</ThemeProvider>
+  <ThemeContext.Provider value={ThemeContext}>{element}</ThemeContext.Provider>
+);
+
+const ThemeWrapper = () => {
+  return <ThemeProvider theme={ThemeContext} />;
+};
+
+// give theme state to styled-components to use
+export const wrapPageElement = ({ element, props }) => (
+  <ThemeContext.Consumer
+    render={value => (
+      <ThemeProvider theme={ThemeContext}>{element}</ThemeProvider>
+    )}
+  />
 );
 
 // ? import primsjs stuff here
