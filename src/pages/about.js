af79e9e2d6a9cf9673c71b8a32f6457981e958e6
Jacob D. Castro <jdcastro.business@gmail.com>
2019-08-31 00:41:02 -0700

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../templates/layout';
+import { Wrapper } from '../styles/index/IndexStyles';
+
+const Index = () => {
+  return (
+    <Layout>
+      <Wrapper>
+        <h1>About Me!</h1>
+      </Wrapper>
+    </Layout>
+  );
+};
+
+export default Index;
