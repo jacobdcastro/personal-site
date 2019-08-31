dd8af2a4f4ea6f041f2df89d32908a72f5705cf3
Jacob D. Castro <jdcastro.business@gmail.com>
2019-08-30 18:04:18 -0700

    add context and state logic to wrapper component

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import ThemeContext from './src/utils/ThemeContext';
-import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './src/styles/__themes';
-
+import ThemeContextWrapper from './src/utils/ThemeContext';
 // provide theme state to entire app
 export const wrapRootElement = ({ element }) => (
-  <ThemeContext.Provider value={ThemeContext}>{element}</ThemeContext.Provider>
-);
-
-const ThemeWrapper = () => {
-  return <ThemeProvider theme={ThemeContext} />;
-};
-
-// give theme state to styled-components to use
-export const wrapPageElement = ({ element, props }) => (
-  <ThemeContext.Consumer
-    render={value => (
-      <ThemeProvider theme={ThemeContext}>{element}</ThemeProvider>
-    )}
-  />
+  <ThemeContextWrapper>{element}</ThemeContextWrapper>
 );
 
 // ? import primsjs stuff here
--- src/utils/ThemeContext.js
+++ src/utils/ThemeContext.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { lightTheme, darkTheme } from '../styles/__themes';
 
-const ThemeContext = React.createContext(
-  lightTheme // default theme
-);
+const ThemeContext = React.createContext({
+  lightTheme,
+  toggleTheme: () => {},
+});
 
-export default ThemeContext;
+// provider component wraps entire app in gatsby-browser.js
+class ThemeContextWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleTheme = () => {
+      this.setState({
+        currentTheme:
+          this.state.currentTheme === lightTheme ? darkTheme : lightTheme,
+      });
+      console.log(this.state.currentTheme);
+    };
+
+    this.state = {
+      currentTheme: lightTheme,
+      toggleTheme: this.toggleTheme,
+    };
+  }
+
+  render() {
+    return (
+      <ThemeContext.Provider value={this.state}>
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
+}
+
+export default ThemeContextWrapper;
+export { ThemeContext };
