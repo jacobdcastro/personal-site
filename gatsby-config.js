0115942457e80187c52bbb3a887a6fba15a55693
Jacob D. Castro <jdcastro.business@gmail.com>
2019-09-23 20:46:36 -0700

    patches

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         lang: `en`,
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
