c9894b6c6ae8187daa1660ac40d8c8231a7f2dd4
Jacob D. Castro <jdcastro.business@gmail.com>
2019-11-19 15:33:10 -0800

    add Helvetica as backup font

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,11 +3,15 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: '1.666',
-  headerFontFamily: ['Montserrat', 'sans-serif'],
+  headerFontFamily: ['Montserrat', 'Helvetica', 'sans-serif'],
   bodyFontFamily: ['Open Sans', 'sans-serif'],
   googleFonts: [
     {
       name: 'Montserrat',
+      styles: ['800', '400'],
+    },
+    {
+      name: 'Helvetica',
       styles: ['800', '400'],
     },
     {
