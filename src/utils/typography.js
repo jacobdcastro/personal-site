46cb51f151f89c70a23a8de7e6fa9132cfc32b54
Jacob D. Castro <jdcastro.business@gmail.com>
2019-11-19 16:27:01 -0800

    remove helvetica backup

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,15 +3,11 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '16px',
   baseLineHeight: '1.666',
-  headerFontFamily: ['Montserrat', 'Helvetica', 'sans-serif'],
+  headerFontFamily: ['Montserrat', 'sans-serif'],
   bodyFontFamily: ['Open Sans', 'sans-serif'],
   googleFonts: [
     {
       name: 'Montserrat',
-      styles: ['800', '400'],
-    },
-    {
-      name: 'Helvetica',
       styles: ['800', '400'],
     },
     {
