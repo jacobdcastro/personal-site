737300cd0250c8296d2aba396852721ac8be9cda
Jacob D. Castro <jdcastro.business@gmail.com>
2019-11-19 18:18:44 -0800

    comment out AJAX request and use netlify default forms

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -3,105 +3,101 @@ import encode from '../../utils/encode';
 import axios from 'axios';
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-  const [formState, setFormState] = useState({
-    submitted: false,
-    success: null,
-  });
-  const { name, email, message } = formData;
-  const { submitting, success } = formState;
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   email: '',
+  //   message: '',
+  // });
+  // const [formState, setFormState] = useState({
+  //   submitted: false,
+  //   success: null,
+  // });
+  // const { name, email, message } = formData;
+  // const { submitting, success } = formState;
 
-  const handleSubmit = async e => {
-    e.preventDefault();
+  // const handleSubmit = async e => {
+  //   e.preventDefault();
 
-    try {
-      const res = await axios({
-        url: '/',
-        baseURL: 'https://jacobdcastro.com/',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Access-Control-Allow-Origin': '*',
-        },
-        data: encode({ 'form-name': 'contactForm', ...formData }),
-      });
-      console.log(res);
-      await setFormState({ submitting: false });
-      setFormState({ success: true });
-    } catch (error) {
-      setFormState({ submitting: false, success: false });
-      console.log(error);
-    }
-  };
+  //   try {
+  //     const res = await axios.post('/.netlify/functions/contact/', {
+  //       'form-name': 'contactForm',
+  //       ...formData,
+  //     });
+  //     console.log(res);
+  //     await setFormState({ submitting: false });
+  //     setFormState({ success: true });
+  //   } catch (error) {
+  //     setFormState({ submitting: false, success: false });
+  //     console.log(error);
+  //   }
+  // };
 
-  const handleChange = e => setFormData({ [e.target.name]: e.target.value });
+  // const handleChange = e => setFormData({ [e.target.name]: e.target.value });
 
-  if (success === null) {
-    return (
-      <form
-        id="contactForm"
-        name="contactForm"
-        onSubmit={e => {
-          setFormState({ submitting: true });
-          handleSubmit(e);
-        }}
-      >
-        <div>
-          <label htmlFor="name">
-            Name<span>*</span>
-          </label>
-          <input
-            id="name"
-            name="name"
-            type="text"
-            value={name}
-            onChange={e => handleChange(e)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">
-            Email<span>*</span>
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="text"
-            value={email}
-            onChange={e => handleChange(e)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="message">
-            Message<span>*</span>
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            type="text"
-            value={message}
-            onChange={e => handleChange(e)}
-            required
-          />
-        </div>
+  // if (success === null) {
+  return (
+    <form
+      id="contactForm"
+      name="contactForm"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      {/* hidden input for netlify bots */}
+      <input type="hidden" name="form-name" value="contact" />
 
-        {submitting ? (
+      <div>
+        <label htmlFor="name">
+          Name<span>*</span>
+        </label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          // value={name}
+          // onChange={e => handleChange(e)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">
+          Email<span>*</span>
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="text"
+          // value={email}
+          // onChange={e => handleChange(e)}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="message">
+          Message<span>*</span>
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          type="text"
+          // value={message}
+          // onChange={e => handleChange(e)}
+          required
+        />
+      </div>
+
+      {/* {submitting ? (
           <button type="submit">Submitting...</button>
-        ) : (
-          <button type="submit">Submit</button>
-        )}
-      </form>
-    );
-  } else if (success) {
-    return <h3>Form submitted! thanks.</h3>;
-  } else {
-    return <h3>Form submission failed</h3>;
-  }
+        ) : ( */}
+      <button type="submit">Submit</button>
+      {/* )} */}
+    </form>
+  );
+  // } else if (success) {
+  //   return <h3>Form submitted! thanks.</h3>;
+  // } else {
+  //   return <h3>Form submission failed</h3>;
+  // }
 };
 
 export default ContactForm;
