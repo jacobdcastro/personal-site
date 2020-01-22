9a6f34053115bc454c44d291677165ad644b0340
Jacob D. Castro <jdcastro.business@gmail.com>
2020-01-21 22:53:08 -0800

    adjust accent color

--- src/styles/layout/__themes.js
+++ src/styles/layout/__themes.js
@@ -7,14 +7,14 @@ const lightTheme = {
   transition: '0.37s',
   bgColor: '#f9f9f9',
   textColor: '#080708',
-  accentColor: '#7e12d1',
+  accentColor: '#7972d2',
 };
 
 const darkTheme = {
   transition: '0.37s',
   bgColor: '#1a1e1c',
   textColor: '#f9f9f9',
-  accentColor: '#7e12d1',
+  accentColor: '#7972d2',
 };
 
 export { darkTheme, lightTheme };
